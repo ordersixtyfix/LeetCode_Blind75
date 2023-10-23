9a52fddf22b1750f5f5449c2fcd3e81b92d5ccad
ordersixtyfix <75178476+ordersixtyfix@users.noreply.github.com>
2023-10-23 16:38:09 +0300

    solution question 217

--- src/solutions/q_121.java
+++ src/solutions/q_121.java
@@ -1,36 +1,36 @@
-package solutions;
-public class q_121 {
+    package solutions;
+    public class q_121 {
 
-    public static int maxProfit(int[] prices) {
+        public static int maxProfit(int[] prices) {
 
-        if (prices.length == 0) {
-            return 0;
+            if (prices.length == 0) {
+                return 0;
+            }
+
+            int maxProfit = 0;
+            int minPrice = prices[0];
+
+            for (int i = 1; i < prices.length; i++) {
+                int currentProfit = prices[i] - minPrice;
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit;
+                }
+                if (prices[i] < minPrice) {
+                    minPrice = prices[i];
+                }
+            }
+
+            return maxProfit;
         }
 
-        int maxProfit = 0;
-        int minPrice = prices[0];
 
-        for (int i = 1; i < prices.length; i++) {
-            int currentProfit = prices[i] - minPrice;
-            if (currentProfit > maxProfit) {
-                maxProfit = currentProfit;
-            }
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            }
+        public static void main(String[] args) {
+
+            int arr = maxProfit(new int[]{7,6,4,3,1});
+            System.out.println(arr);
+
+
         }
 
-        return maxProfit;
-    }
-
-
-    public static void main(String[] args) {
-
-        int arr = maxProfit(new int[]{7,6,4,3,1});
-        System.out.println(arr);
-
 
     }
-
-
-}
---
+++ src/solutions/q_217.java
@@ -0,0 +1,36 @@
+package solutions;
+
+import java.util.HashSet;
+
+public class q_217 {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        boolean exist = containsDuplicate(new int[]{7,6,4,3,1});
+        System.out.println(exist);
+
+    }
+}
